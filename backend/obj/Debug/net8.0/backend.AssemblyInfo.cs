290e5a0817948cd076115b4723e8ab036c1b3870
IlyaSkalb4 <pagany10@gmail.com>
2024-08-08 10:11:14 +0200

    Small changes in _Layout.cshtml.

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a401fc847e21ab8bfdaca88686ca5598436304")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da060c446f646767d6eb7e193f3f8a89d05d0f76")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
